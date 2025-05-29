974914460b316ed7c19b38607f5f50dd2d23e855
8848 <382596868@qq.com>
2025-05-29 10:16:37 +0800

    补充实验结果

--- 01_IterativeRefinement.py
+++ 01_IterativeRefinement.py
@@ -1,5 +1,6 @@
 import torch
 from diffusers import DDPMPipeline
+import matplotlib.pyplot as plt
 
 
 def get_device():
@@ -13,6 +14,31 @@ print(device)
 image_pipe = DDPMPipeline.from_pretrained("google/ddpm-celebahq-256")
 image_pipe.to(device)
 
+# # Sample an image
+
+# img = image_pipe().images[0]
+
+
+# plt.figure()
+# plt.imshow(img)
+# plt.axis('off') # 关闭坐标轴
+# plt.savefig('/data/tyh/ws/diffusion_from_diffusers/img/celebahq.png')
+
+
+
 # Sample an image
-image_pipe().images[0]
+batchsize = 8
+imgs = image_pipe(batch_size = batchsize).images
+"""使用Matplotlib创建网格图"""
+rows, cols = 2,4
+fig, axes = plt.subplots(rows, cols, figsize=(12, 6))
+axes = axes.flatten()
+
+for i, img in enumerate(imgs):
+    axes[i].imshow(img)
+    axes[i].axis('off')  # 隐藏坐标轴
+
+plt.tight_layout()  # 调整布局
+plt.savefig('/data/tyh/ws/diffusion_from_diffusers/img/delebahqs.png', dpi=300, bbox_inches='tight')
+
 
